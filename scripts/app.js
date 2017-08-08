1bdcacea2996c1939af3a9ca69946e7b89d8bc70
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-08 15:44:56 +0200

    Add quiz window

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,24 @@
 var vid = document.getElementById("vid");
-var seekVar;
-vid.addEventListener("click", function (e) {
+var quizArrayObj = [
+    {
+        quiz: "ما الحرف الدي سنتكلم عنه؟",
+        ans: "حرف",
+        correctAns: "الفاء",
+    }
+]
+var seekVar,
+    quizInterval;
+vid.addEventListener("click", function(e){
     e.target.pause();
     $(".play-btn").show();
     clearInterval(seekVar);
-});
+})
 
-$(".play-btn").click("click", function () {
+$(".play-btn").click("click", function(){
     $(this).hide();
     vid.play();
     seekVar = setInterval(seekFun, 200);
+    quizInterval = setInterval(quizFun, 10);
 });
 
 function seekFun() {
@@ -17,7 +26,20 @@ function seekFun() {
         width: "+=10px"
     }, 200);
 
-    if (parseInt($(".inside-seekbar").css("width")) >= 1140) {
+    if(parseInt($(".inside-seekbar").css("width")) >= 1140){
         clearInterval(seekVar);
+        clearInterval(quizInterval);
+        $(".quiz").text(quizArrayObj[0].quiz);
+        $(".quiz-div").css("transform", "scale(1)");
+        
+    }
+}
+
+function quizFun(){
+    if(vid.currentTime >= 4.6 && vid.currentTime <= 4.7){
+        vid.pause();
+        clearInterval(seekVar);
+        clearInterval(quizInterval);
+        
     }
 }
