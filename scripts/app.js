6626c5bc5a960347baa3748ac65ee9071f907f6b
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-08 16:30:36 +0200

    Add check answer function

--- scripts/app.js
+++ scripts/app.js
@@ -5,16 +5,18 @@ var quizArrayObj = [
         ans: "حرف",
         correctAns: "الفاء",
     }
-]
+];
+var score = 0,
+    currentQuiz = 0;
 var seekVar,
     quizInterval;
-vid.addEventListener("click", function(e){
+vid.addEventListener("click", function (e) {
     e.target.pause();
     $(".play-btn").show();
     clearInterval(seekVar);
 })
 
-$(".play-btn").click("click", function(){
+$(".play-btn").click("click", function () {
     $(this).hide();
     vid.play();
     seekVar = setInterval(seekFun, 200);
@@ -26,20 +28,19 @@ function seekFun() {
         width: "+=10px"
     }, 200);
 
-    if(parseInt($(".inside-seekbar").css("width")) >= 1140){
+    if (parseInt($(".inside-seekbar").css("width")) >= 1140) {
         clearInterval(seekVar);
         clearInterval(quizInterval);
-       
     }
 }
 
-function quizFun(){
-    if(vid.currentTime >= 4.6 && vid.currentTime <= 4.7){
+function quizFun() {
+    if (vid.currentTime >= 4.6 && vid.currentTime <= 4.7) {
         vid.pause();
         clearInterval(seekVar);
         clearInterval(quizInterval);
-         $(".quiz").text(quizArrayObj[0].quiz);
-        $(".ans-span").text(quizArrayObj[0].ans);
+        $(".quiz").text(quizArrayObj[currentQuiz].quiz);
+        $(".ans-span").text(quizArrayObj[currentQuiz].ans);
         $(".quiz-div").animate({
             width: "500px",
             height: "300px",
@@ -47,3 +48,24 @@ function quizFun(){
         }, 200);
     }
 }
+
+$(".check-ans-btn").click(function () {
+    if ($(".entered-ans").val().replace(/\s/g, '') == quizArrayObj[currentQuiz].correctAns) {
+
+        score++;
+        $(".score-span").text(score);
+
+    }
+    setTimeout(function () {
+        $(".quiz-div").css({
+            "width": "0",
+            "height": "0",
+            "padding": "0"
+        });
+        vid.play();
+        setTimeout(function () {
+            seekVar = setInterval(seekFun, 200);
+            quizInterval = setInterval(quizFun, 10);
+        }, 500);
+    }, 300);
+});
