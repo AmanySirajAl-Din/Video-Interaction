c41a350b278b1dcc8adcb9801fa904601b46994d
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-08 15:43:57 +0200

    Add play-btn's play & pause functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,23 @@
-
+var vid = document.getElementById("vid");
 var seekVar;
+vid.addEventListener("click", function (e) {
+    e.target.pause();
+    $(".play-btn").show();
+    clearInterval(seekVar);
+});
+
+$(".play-btn").click("click", function () {
+    $(this).hide();
+    vid.play();
+    seekVar = setInterval(seekFun, 200);
+});
 
 function seekFun() {
     $(".inside-seekbar").animate({
         width: "+=10px"
     }, 200);
 
-    if(parseInt($(".inside-seekbar").css("width")) >= 1140){
+    if (parseInt($(".inside-seekbar").css("width")) >= 1140) {
         clearInterval(seekVar);
     }
 }
