1a9fbdcc053ea41f3877ef6cfabacc0faa4225e8
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-09 13:07:13 +0200

    Add another question type

--- scripts/app.js
+++ scripts/app.js
@@ -8,9 +8,19 @@ var compArrayObj = [
     }
 ];
 
-
+var mcqArrayObj = [
+    {
+        quiz: "ما المكان المتبقي لحرف الفاء؟",
+        ans1: "حرف الفاء أول الكلمة",
+        ans2: "حرف الفاء أخر الكلمة",
+        ans3: "حرف الفاء وسط الكلمة",
+        choicesNum: 3,
+        correctAns: "ans2"
+    }
+];
 var score = 0,
-    currentQuiz = 0;
+    currentCompQuiz = 0,
+    currentMcqQuiz = 0;
 var seekVar,
     quizInterval;
 
@@ -42,26 +52,58 @@ function seekFun() {
 
 function quizFun() {
     if (vid.currentTime >= 4.6 && vid.currentTime <= 4.7) {
-        $(".quiz").text(compArrayObj[currentQuiz].quiz);
-        $(".ans-span").text(compArrayObj[currentQuiz].ans);
+        $(".quiz").text(compArrayObj[currentCompQuiz].quiz);
+        $(".ans-span").text(compArrayObj[currentCompQuiz].ans);
         showQuizDiv();
-        quizTypeArr[currentQuiz].show();
+        quizTypeArr[currentCompQuiz + currentMcqQuiz].show();
+    } else if (vid.currentTime >= 15.4 && vid.currentTime <= 15.5) {
+        $(".quiz").text(mcqArrayObj[currentMcqQuiz].quiz);
+        for (var i = 1; i <= mcqArrayObj[currentMcqQuiz].choicesNum; i++) {
+            $(".ans-span" + i).text(mcqArrayObj[currentMcqQuiz]["ans" + i]);
+        }
+        $("input[name=ans-radio" + (currentMcqQuiz+1)  + "]").each(function(index){
+            $(this).val(mcqArrayObj[currentMcqQuiz]["ans" + (index+1)]);
+        });
+        showQuizDiv();
+        quizTypeArr[currentCompQuiz + currentMcqQuiz].show();
     }
 }
 
 $(".comp-check").click(function () {
-    if ($(".entered-ans").val().replace(/\s/g, '') == compArrayObj[currentQuiz].correctAns) {
+    $(this).hide();
+    if ($(".entered-ans").val().replace(/\s/g, '') == compArrayObj[currentCompQuiz].correctAns) {
+       
         score++;
         $(".score-span").text(score);
         $(".entered-ans").css("color", "#019b01");
         $(".feedback").addClass("correct-ans");
-    }else{
+        $(".feedback").show();
+    } else {
         $(".entered-ans").css("color", "red");
         $(".feedback").addClass("wrong-ans");
+        $(".feedback").show();
     }
-    currentQuiz++;
+    currentCompQuiz++;
 
     setTimeout(hideQuizDiv, 500);
+});
+
+$(".mcq-check").click(function () {
+    $(this).hide();
+    if ($('input[name=ans-radio'+ (currentMcqQuiz+1) +']:checked').val() ==  mcqArrayObj[currentMcqQuiz][mcqArrayObj[currentMcqQuiz].correctAns]) {
+        score++;
+        $(".score-span").text(score);
+        $('input[name=ans-radio'+ (currentMcqQuiz+1) +']:checked').prev().css("color", "#019b01");
+        $(".feedback").addClass("correct-ans");
+        $(".feedback").show();
+    } else {
+        $('input[name=ans-radio'+ (currentMcqQuiz+1) +']:checked').prev().css("color", "red");
+        $(".feedback").addClass("wrong-ans");
+        $(".feedback").show();
+    }
+    currentMcqQuiz++;
+
+    setTimeout(hideQuizDiv, 1000);
 });
 
 function hideQuizDiv() {
@@ -71,6 +113,11 @@ function hideQuizDiv() {
             "height": "0",
             "padding": "0"
         });
+        $(".feedback").removeClass(".correct-ans");
+        $(".feedback").removeClass(".wrong-ans");
+        $(".feedback").hide();
+        $(".complete-quiz").hide();
+        $(".mcq-quiz").hide();
         vid.play();
         setTimeout(function () {
             seekVar = setInterval(seekFun, 200);
@@ -80,10 +127,12 @@ function hideQuizDiv() {
 }
 
 function showQuizDiv() {
+    $(".comp-check").show();
+    $(".mcq-check").show();
     vid.pause();
     clearInterval(seekVar);
     clearInterval(quizInterval);
-    
+
     $(".quiz-div").animate({
         width: "500px",
         height: "300px",
